70041d496188eac87bb9fd6a7f1c31fedc791a1a
pawan-2610 <pawan@rimiktech.com>
2025-12-08 16:52:10 +0530

    googlefont

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -3,6 +3,7 @@ import "./Login.css"
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useAuth } from '../context/AuthContext';
+
 function Login(){
     const[email, setemail]=useState("");
     const[password, setpassword]=useState("");
@@ -13,40 +14,40 @@ function Login(){
     function handleLogin(e){
         const {name,value}=e.target;
         if(name==="email"){
-        setemail(value);
+            setemail(value);
         }
         if(name==="password"){
-            setpassword(e.target.value)
+            setpassword(value)
         }
-        
     }
+
     function handleSubmit(e){
         e.preventDefault();
-     setsubmit(true)
-     login({ email });
-     navigate("/landing")
+        setsubmit(true)
+        login({ email });
+        navigate("/landing")
     }
     
     return(
         <div>
-            <form className='loginPage1' onSubmit={handleSubmit} >
+            <form className='loginPage1' onSubmit={handleSubmit}>
+                <h1 className="vanira-title">Vanira</h1>
                 <input type='email'
-                name='email'  
-                autoComplete="off"
-                value={email}
-                onChange={handleLogin}
-                placeholder='please enter your email'/>
+                    name='email'
+                    autoComplete="off"
+                    value={email}
+                    onChange={handleLogin}
+                    placeholder='please enter your email'/>
 
-
-                <input type='password' 
-                name='password'
-                value={password}
-                autoComplete="off"
-                onChange={handleLogin}
-                placeholder='please enter your password'/>
-
+                <input type='password'
+                    name='password'
+                    autoComplete="off"
+                    value={password}
+                    onChange={handleLogin}
+                    placeholder='please enter your password'/>
 
                 <button type="submit" className='btn-1'>Log in</button>
+
                 <div className='forgotten-ps'>
                     <a href=''>Forgotten password?</a>
                 </div>
--- src/components/loginpage/LoginPage.jsx
+++ src/components/loginpage/LoginPage.jsx
@@ -1,18 +1,31 @@
 import React from "react";
 import Slideer from "../slider/Slideer";
 import Login from "../Login/Login";
-function LoginPage(){
-    return(
-        <div className="firstPage">
-            <div className="row">
-            <div className="col-md-6">
-                <Slideer/>
+import "./LoginPage.css";
+import bgImage from "../../assets/backgroundimage/pig-pictures-6025jrkvm8tyvp95.jpg"; 
+
+function LoginPage() {
+  return (
+    <div
+      className="firstPage"
+      style={{
+        backgroundImage: `url(${bgImage})`,
+        height: "100vh",
+        backgroundSize: "cover",
+        backgroundPosition: "center"
+      }}
+    >
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-md-12">
+            <div className="loginForm">
+            <Login />
             </div>
-            <div className="col-md-6">
-           <Login/>
-            </div>
-            </div>
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
-export default LoginPage
+
+export default LoginPage;
